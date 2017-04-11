05058070d62e7048e5dda10e49ab02214cbc3967
HackeZ <767110505@qq.com>
2017-04-11 15:10:36 +0800

    add basic type encode

--- encoder.go
+++ encoder.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"runtime"
+	"time"
 )
 
 type Encoder struct {
@@ -19,11 +20,11 @@ func init() {
 	log.SetPrefix(filename + "\n")
 }
 
-// encodeBinary
+// EncodeBinary
 // binary ::= [x62(b)]  b1 b0 <binary-data> binary  # non-final chunk
 //        ::= [x42(B)]  b1 b0 <binary-data>         # final chunk
 //        ::= [x20-x2f] <binary-data>               # length less than 15
-func encodeBinary(v []byte) (b []byte, err error) {
+func EncodeBinary(v []byte) (b []byte, err error) {
 	if length := len(v); length <= 15 {
 		b = append(b, 0x20+byte(length))
 		b = append(b, v...)
@@ -52,5 +53,113 @@ func encodeBinary(v []byte) (b []byte, err error) {
 	}
 	b = append(b, packB...)
 	b = append(b, v[offset:]...)
+	return
+}
+
+// EncodeBool encode bool
+// boolean ::= 'T'
+//         ::= 'F'
+func EncodeBool(f bool) (b []byte, err error) {
+	if true == f {
+		b = append(b, 'T')
+		return
+	}
+	b = append(b, 'F')
+	return
+}
+
+// EncodeDate encode date
+// date    ::= 'd' b7 b6 b5 b4 b3 b2 b1 b0
+func EncodeDate(date time.Time) (b []byte, err error) {
+	tmpB := make([]byte, 0, 8)
+	if tmpB, err = packInt64(date.UnixNano() / 1e6); err != nil {
+		return
+	}
+	b = append(b, 'd')
+	b = append(b, tmpB...)
+	return
+}
+
+// EncodeFloat64 encode float64
+// double    ::= 'D' b7 b6 b5 b4 b3 b2 b1 b0
+func EncodeFloat64(v float64) (b []byte, err error) {
+	tmpB := make([]byte, 0, 8)
+	if tmpB, err = packFloat64(v); err != nil {
+		return
+	}
+	b = append(b, 'D')
+	b = append(b, tmpB...)
+	return
+}
+
+// EncodeInt32 encode int
+// int    ::= 'I' b3 b2 b1 b0
+func EncodeInt32(v int32) (b []byte, err error) {
+	tmpB := make([]byte, 0, 4)
+	if tmpB, err = packInt32(v); err != nil {
+		return
+	}
+
+	b = append(b, 'I')
+	b = append(b, tmpB...)
+	return
+}
+
+// EncodeLong encode long
+// long    ::= 'L' b7 b6 b5 b4 b3 b2 b1 0
+func EncodeLong(v int64) (b []byte, err error) {
+	tmpB := make([]byte, 0, 8)
+	if tmpB, err = packInt64(v); err != nil {
+		return
+	}
+
+	b = append(b, 'L')
+	b = append(b, tmpB...)
+	return
+}
+
+// EncodeNull encode null
+// null    ::= 'N'
+func EncodeNull(v interface{}) (b []byte, err error) {
+	b = append(b, 'N')
+	return
+}
+
+// EncodeString encode string
+// string    ::= R(x52) b1 b0 <utf8-data> string  # non-final chunk
+//           ::= S(x53) b1 b0 <utf8-data>         # string of length 0-65535
+//           ::= [x00-x1f] <utf8-data>            # string of length 0-31
+//           ::= [x30-x33] b0 <utf8-data>         # string of length 0-1023
+func EncodeString(s string) (b []byte, err error) {
+	length, strOffset, subLen := len(s), 0, 0
+	byteS := []byte(s)
+
+	for length > ChunkSize {
+		subLen = ChunkSize
+
+		tmpB, err := packUInt16(uint16(subLen))
+		if err != nil {
+			return b, err
+		}
+		b = append(b, 'R')
+		b = append(b, tmpB...)
+		b = append(byteS[strOffset : strOffset+subLen])
+
+		length -= subLen
+		strOffset += subLen
+	}
+
+	if length <= 31 {
+		// short strings
+		b = append(b, byte(length))
+	} else if length <= 1023 {
+		b = append(b, byte(48+(length>>8)))
+		b = append(b, byte(length))
+	} else {
+		b = append(b, 'S')
+		b = append(b, byte(length>>8))
+		b = append(b, byte(length))
+	}
+	b = append(b, byteS[strOffset:]...)
 	return
 }
--- encoder_test.go
+++ encoder_test.go
@@ -4,6 +4,32 @@ import (
 	"testing"
 )
 
-func EncodeBytesTest(t *testing.T) {
+func TestEncodeBytes(t *testing.T) {
+	lessThan15 := make([]byte, 0, 20)
+	lessThan15 = append(lessThan15, 'a', 'b', 'c', 'd', 'e')
+	result, err := EncodeBinary(lessThan15)
+	if err != nil {
+		t.Error("encode binary failed, lessThan15:", lessThan15, " error:", err)
+	}
+	t.Log("result of lessThan15: ", result)
 
+	lessThan64k := make([]byte, 0, 0x8000)
+	for i := 0; i < 0x7999; i++ {
+		lessThan64k = append(lessThan64k, 'a')
+	}
+	result, err = EncodeBinary(lessThan64k)
+	if err != nil {
+		t.Error("encode binary failed, lessThan64k:", lessThan64k, " error:", err)
+	}
+	t.Log("result of lessThan64k: ", result)
+
+	moreThan64k := make([]byte, 0, 0x8001)
+	for i := 0; i <= 0x8000; i++ {
+		moreThan64k = append(moreThan64k, 'a')
+	}
+	result, err = EncodeBinary(moreThan64k)
+	if err != nil {
+		t.Error("encode binary failed, moreThan64k:", moreThan64k, " error:", err)
+	}
+	t.Log("result of lessThan64k: ", result)
 }
